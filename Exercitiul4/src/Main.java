60d0b51dfcfd4fc81d4ab95b8f7aeee932f8deaf
Adrian.Paraschiv <adrianmarius.paraschiv@gmail.com>
2020-05-03 12:04:20 +0300

    am terminat cu probleme exercitiul 4

--- Exercitiul4/src/Main.java
+++ Exercitiul4/src/Main.java
@@ -4,4 +4,26 @@ public class Main {
     // sa se numere toate cuvintele care au lungimea mai mare decat 2, contin caracterul "i" si caracterul "i" se afla pe o pozitie para
     // exemplu Matei
 
+    public static void main(String[] argns){
+        String text = " hfdsbkj fhsdbfk hferqw ufcb uqhoib wr";
+
+        int numarCuvinte = numarCuvinte(text);
+        System.out.println("numarul de cuvinte este" + numarCuvinte);
+
+    }
+
+    public static int numaraCuvinte(String text){
+
+        int numarCuvinte = 0;
+        String[] cuvinte = text.split( regex: "");
+
+        for(int cursor = 0; cursor < cuvinte.length; cursor ){
+            if(cuvinte[cursor].length() > 2 && cuvinte[cursor].contains("i") && cuvinte[cursor].indexOf("i") % 2 == 0){
+                numarCuvinte++;
+            }
+        }
+        return numarCuvinte;
+    }
+
+
 }

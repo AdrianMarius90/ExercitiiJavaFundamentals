46cce00baf17bf63f4aa2334e2793bb508620e08
Adrian.Paraschiv <adrianmarius.paraschiv@gmail.com>
2020-05-03 11:46:40 +0300

    exercitiul 4 neterminat

---
+++ Exercitiul4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    // se da un text
+    // sa se numere toate cuvintele care au lungimea mai mare decat 2, contin caracterul "i" si caracterul "i" se afla pe o pozitie para
+    // exemplu Matei
+
+}
